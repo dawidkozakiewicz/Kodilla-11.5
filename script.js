93c0b95031a1d93bb7db25c779d082b1fb68f233
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-02-16 08:59:43 +0100

    Creating js

---
+++ script.js
@@ -0,0 +1,27 @@
+$(function() {
+  function Button(text) {
+    this.text = text || 'Hello';
+  }
+
+  Button.prototype = {
+    create: function() {
+      var self = this;
+      this.$element = $('<button>');
+      this.$element.text(this.text);
+      this.$element.click(function() {
+        alert(self.text);
+      });
+      $('body').append(this.$element);
+    }
+  };
+
+  var btn1 = new Button('Hello!');
+  var btn2 = new Button('Priviet!');
+  var btn3 = new Button('Siema!');
+  var btn4 = new Button('Hola!');
+  btn1.create();
+  btn2.create();
+  btn3.create();
+  btn4.create();
+  
+});
